c4ecb54aeb7ab3b0e4d4b71a44b2354791fc890d
pranavakkian <pranavakkian@gmail.com>
2019-12-27 19:44:21 +0530

    modified package.json and added google adsense.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AdSense from "react-adsense";
 import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 import "semantic-ui-css/semantic.min.css";
@@ -8,6 +9,15 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactGA.initialize("UA-124182431-4");
 ReactGA.pageview(window.location.pathname + window.location.search);
+
+<AdSense.Google
+  client="ca-pub-6964520538689912"
+  slot="7806394673"
+  style={{ display: "block" }}
+  format="auto"
+  responsive="true"
+  layoutKey="-gw-1+2a-9x+5c"
+/>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
